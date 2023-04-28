4f4f8059f80e94a970869877762f13184124a0f8
anusha <anushag2k16@gmail.com>
2023-04-28 16:00:22 +0530

    committing to roboshop-shell

--- loops/loop.tf
+++ loops/loop.tf
@@ -4,9 +4,7 @@ default = ["banana","mango","apple","orange","grapes","avacodo"]
 resource "null_resource" "test" {
 
 count = length(var.fruits)
-tags
-
-{
+tags = {
 name = var.fruits[count.index]
 }
 
