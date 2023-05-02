90d457ac866c6f44c6d7c2afb22b1df50ade7686
anusha <anushag2k16@gmail.com>
2023-05-02 16:22:02 +0530

    committing to roboshop-shell

--- loops/foreach.tf
+++ loops/foreach.tf
@@ -1,14 +1,14 @@
 variable "dictionary" {
 default = {
-telugu {
+telugu ={
  price = 150
  name"tdic"
 }
- hindi {
+ hindi= {
 price =180
  name="hd"
  }
- english {
+ english ={
  price = 2150
  name = "ed"
  }
@@ -22,10 +22,6 @@ for_each = var.dictionary
 
 provisioner "local-exec" {
 
-command = "echo ${each.key} ${each.value["price"] ${each.value["name"]}"
-
+command = "echo ${each.key} -${each.value["price"] -${each.value["name"]}"
 }
-
-
-
 }
