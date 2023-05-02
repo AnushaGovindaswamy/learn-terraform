0854c7fab146974b9252b9740b189b1f3e01640d
anusha <anushag2k16@gmail.com>
2023-05-02 16:06:45 +0530

    committing to roboshop-shell

---
+++ for_eachloops.tf
@@ -0,0 +1,31 @@
+variable "dictionary" {
+default = {
+telugu {
+ price = 150
+ name"tdic"
+}
+ hindi {
+price =180
+ name="hd"
+ }
+ english {
+ price = 2150
+ name = "ed"
+ }
+}
+
+}
+
+resource "null resource" "sample" {
+
+for_each = var.dictionary
+
+provisioner "local-exec" {
+
+command = "echo ${each.key} ${each.value[price] ${each.value[name]}"
+
+}
+
+
+
+}
